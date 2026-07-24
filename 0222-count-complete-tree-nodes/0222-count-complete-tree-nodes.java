a80bcea6c5da0f774879f0bf7d0d616e8ee7135c
Pradyumn-Chamoli <pradyumnchamoli18@gmail.com>
2026-07-24 22:35:49 +0530

    Time: 0 ms (100%), Space: 49.5 MB (29.99%) - LeetHub

---
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    private int findLeftHeight(TreeNode node){
+        int height = 0;
+
+        while(node!=null){
+            height++;
+            node = node.left;
+        }
+        return height;
+    }
+
+    private int findRightHeight(TreeNode node){
+        int height=0;
+
+        while(node!=null){
+            height++;
+            node = node.right;
+        }
+
+        return height;
+    }
+    public int countNodes(TreeNode root) {
+
+        if(root==null){
+            return 0;
+        }
+
+        int lh = findLeftHeight(root);
+        int rh = findRightHeight(root);
+
+        if(lh==rh){
+            return (1<<lh)-1;
+        }
+
+        return 1+countNodes(root.left) + countNodes(root.right);
+    }
+}
