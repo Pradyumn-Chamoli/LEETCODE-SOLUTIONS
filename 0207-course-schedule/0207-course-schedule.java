21c693f271b66ca4dbbd8f6b8fdc86b2448139cb
Pradyumn-Chamoli <pradyumnchamoli18@gmail.com>
2026-09-06 16:03:32 +0530

    Time: 8 ms (24.36%), Space: 47.7 MB (5.37%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,47 @@
+
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int N = numCourses;
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i=0 ; i<N ; i++){
+            adj.add(new ArrayList<>());
+        }
+
+        int m=prerequisites.length;
+        for(int i=0 ; i<m ; i++){
+            adj.get(prerequisites[i][1]).add(prerequisites[i][0]);
+        }
+
+        int []indegree = new int[N];
+        for(int i=0 ; i<N ; i++){
+            for(int it : adj.get(i)){
+                indegree[it]++;
+            }
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0 ; i<N ; i++){
+            if(indegree[i]==0){
+                q.add(i);
+            }
+        }
+
+
+        List<Integer> topo = new ArrayList<>();
+
+        while(!q.isEmpty()){
+            int node = q.poll();
+            topo.add(node);
+            for(int it : adj.get(node)){
+                indegree[it]--;
+                if(indegree[it]==0){
+                    q.add(it);
+                }
+            }
+        }
+
+        if(topo.size()==N) return true;
+        return false;
+        
+    }
+}
